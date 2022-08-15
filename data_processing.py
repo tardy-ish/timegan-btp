13d6460165cadda83484bce3f84f6983c38b54c4
tardy-ish <spidey.ds@gmail.com>
2022-08-15 20:14:34 +0530

    image_crop_intr

--- data_processing.py
+++ data_processing.py
@@ -9,7 +9,16 @@ def import_data(folder_path,n = -1):
         if n != -1 and i >= n:
             break
         i += 1
-        image = Image.open(f"{folder_path}/{img_l}").convert("L").resize((1024,1024))
+        image = Image.open(f"{folder_path}/{img_l}")
+        image = cropImg(image)
+        image = image.convert("L").resize((1024,1024))
         image = np.atleast_3d(image)
         data.append(image)
     return np.array(data)
+
+def cropImg(image):
+    left = 4
+    top = 84
+    right = left + 1200
+    bottom = top + 1200
+    return image.crop((left, top, right, bottom))
