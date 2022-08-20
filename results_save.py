0df5400dc40811ea1bde7f877356f56f94ecf4b3
tardy-ish <spidey.ds@gmail.com>
2022-08-20 10:12:22 +0530

    res

--- results_save.py
+++ results_save.py
@@ -7,7 +7,7 @@ from math import ceil
 def result_compile_mnist(data,fld,n,s):
     if not os.path.exists(f"timeGAN_results/mnist/{fld}"):
         os.mkdir(f"timeGAN_results/mnist/{fld}")    
-    
+    print(data.shape)
     for k in range(n):
         bckg = Image.new(mode="RGB",size=(410,160),color=(255,255,255))
         y = 10
@@ -18,7 +18,7 @@ def result_compile_mnist(data,fld,n,s):
                 if i*8 + j >= s:
                     k = True
                     break
-                print(i*8 + j)
+                # print(i*8 + j)
                 img = data[k][i*8 + j].reshape((28,28))
                 img = Image.fromarray(img)
                 bckg.paste(img.resize((40,40)),(x,y))
